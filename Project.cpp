c699eb97927f00f4176f104e23f96a7c0e96f974
McGengar <macwegrzyn@gmail.com>
2024-12-09 17:08:10 +0100

    Dodaj pliki projektów.

---
+++ Project.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "SFML/Graphics.hpp"
+int main()
+{
+    
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Test");
+    sf::Event e;
+    std::cout << "Hello World!\n";
+
+    while (window.isOpen())
+    {
+        while (window.pollEvent(e)) 
+        {
+            if (e.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+    }
+    
+    
+
+
+}
