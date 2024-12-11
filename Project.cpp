31ee77a9db7e21b1f71cf1cecc65e84560deef4a
McGengar <macwegrzyn@gmail.com>
2024-12-11 22:51:12 +0100

    simple strutcure, adding keyboard handling and test images

--- Project.cpp
+++ Project.cpp
@@ -1,24 +1,73 @@
 ﻿#include <iostream>
 #include "SFML/Graphics.hpp"
+#include <string>
+
+using namespace std;
+
+const int SCREEN_HEIGHT = 768;
+const int SCREEN_WIDTH = 1024;
+
+class DrawableObject {
+public:
+    void Draw();
+    const char* texture_path;
+};
+
+class Cat : DrawableObject {
+public:
+    int number_of_states;
+    int current_state;
+    void OnUpdate();
+};
+
+class VarCat : Cat {
+public:
+    const char* texture_path = "resources/varcat_2";
+};
+
+
 int main()
 {
-    
-    sf::RenderWindow window(sf::VideoMode(800, 800), "Test");
-    sf::Event e;
-    std::cout << "Hello World!\n";
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SynCats");
+    string message = "Hello World";
+    string display = "";
+    int index = 0;
+    //std::cout << "Hello World!\n";
+
+    sf::Clock clock;
+    sf::Time time;
+    float DeltaTime;
+
+    window.setKeyRepeatEnabled(false);
 
     while (window.isOpen())
     {
-        while (window.pollEvent(e)) 
+        sf::Event event;
+        time = clock.getElapsedTime();
+        DeltaTime = time.asSeconds();
+        while(window.pollEvent(event)) 
         {
-            if (e.type == sf::Event::Closed)
+
+            switch (event.type)
             {
+            case sf::Event::Closed:
                 window.close();
+                break;
+            case sf::Event::KeyPressed:
+                switch (event.key.code) {
+                case sf::Keyboard::Return:
+                    display += message[index];
+                    index++;
+                    system("cls");
+                    cout << display;
+                    break;
+                }
             }
+            
         }
+        clock.restart();
+
+        window.display();
     }
     
-    
-
-
 }
